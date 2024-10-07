0a95352310867efa3d6c6ca95d448f1dec4cae15
jobnovice <jobkebede47@gmail.com>
2024-10-07 22:20:38 +0300

    aight

--- 0x00-ES6_basic/0-constants.test.js
+++ 0x00-ES6_basic/0-constants.test.js
@@ -1,15 +1,18 @@
 import { taskFirst, getLast, taskNext } from './0-constants';
 
-describe('Test 0-constants.js functions', () => {
-    test('taskFirst should return the correct string', () => {
-        expect(taskFirst()).toBe('I prefer const when I can.');
-    });
-    
-    test('getLast should return the correct strign', () => {
-        expect(getLast()).toBe('is okay');
-    });
+describe('test 0-constants.js functions', () => {
+  it('taskFirst should return the correct string', () => {
+    expect.hasAssertions();
+    expect(taskFirst()).toBe('I prefer const when I can.');
+  });
 
-    test('taskNext should return the correct concatenated string', () => {
-        expect(taskNext()).toBe('but sometimes let is okay');
-    });
-})
+  it('getLast should return the correct strign', () => {
+    expect.hasAssertions();
+    expect(getLast()).toBe(' is okay');
+  });
+
+  it('taskNext should return the correct concatenated string', () => {
+    expect.hasAssertions();
+    expect(taskNext()).toBe('But sometimes let is okay');
+  });
+});
