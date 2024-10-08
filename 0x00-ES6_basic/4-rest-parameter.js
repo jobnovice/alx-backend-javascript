b44bb7731f7dd344570e6960345bb877d8d42ff0
jobnovice <jobkebede47@gmail.com>
2024-10-08 22:39:30 +0300

    right

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let count = 0;
-  let arg;
-  // eslint-disable-next-line no-unused-vars
-  for (arg of args) count += 1;
-  return count;
+  return args.length;
 }
